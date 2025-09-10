a802ebf918a3aa81ad6e205ec3d41e05f44bd079
JCCallisto <jcchronokeeper@gmail.com>
2025-09-10 19:41:53 -0400

    Node Module files

---
+++ TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,23 @@
+import { Subjects } from './Teacher';
+import { Subjects as SubjectClass } from './Subject';
+
+export namespace Subjects {
+  // Declaration merging - adding experienceTeachingJava to Teacher interface
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for Java";
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingJava && this.teacher.experienceTeachingJava > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return "No available teacher";
+      }
+    }
+  }
+}
